9394b215da626f57aa7ba1436fda9dd6f44ab57a
Aishah <aishah122005@gmail.com>
2024-04-24 10:02:10 -0500

    l

--- Pizza Hunter/LocationManager.swift
+++ Pizza Hunter/LocationManager.swift
@@ -1,8 +1,19 @@
 //
 //  LocationManager.swift
-//  Pizza Hunter
+//  Za Hunter Hackwich
 //
 //  Created by Aishah Siraj on 4/11/24.
 //
 
 import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    var locationManager = CLLocationManager()
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
